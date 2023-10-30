f8426f7e27e36fa293782b25450712a3c928c8b3
xiaopihong <260094892@qq.com>
2023-10-30 23:08:27 +0800

    build(deps): 引入antd

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "antd";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
 				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
+				<Button>antd按钮</Button>
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
