612695629060127d8e0c92fd625d47a241974276
xiaopihong <260094892@qq.com>
2023-10-30 23:42:12 +0800

    build(deps): 引入react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "antd/dist/reset.css";
+import { BrowserRouter } from "react-router-dom"; // 使用历史模式路由实现跳转
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
+	<BrowserRouter>
 		<App />
-	</React.StrictMode>
+	</BrowserRouter>
 );
