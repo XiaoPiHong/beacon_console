f0ab1b5544c9db98dd426e744c9e10d841d2a40a
xiaopihong <260094892@qq.com>
2023-10-31 22:36:15 +0800

    docs(other): 规范项目目录结构

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import "antd/dist/reset.css";
 import { BrowserRouter } from "react-router-dom"; // 使用历史模式路由实现跳转
+import "@/assets/styles/index.less";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<BrowserRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), eslintPlugin()]
+	plugins: [react(), eslintPlugin()],
+	resolve: { alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }] }
 });
