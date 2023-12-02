bb84df89049f5d970364788196575f9bf1acb8c0
CokeBeliever <cokebeliever@163.com>
2023-12-03 02:08:54 +0800

    chore(other): 配置调整

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), eslintPlugin()],
-	resolve: { alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }] }
+	resolve: { alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }] },
+	server: {
+		proxy: {
+			"/pms": {
+				target: "https://www.fastmock.site/mock/f18bce957e91793c339504b2f17b384e",
+				changeOrigin: true
+			}
+		}
+	}
 });
