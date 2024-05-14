d6bf2c6423b9b76f0816e0f76da1ee9563d0e1b1
zhouzhenhong <xiaopihong0325@qq.com>
2024-05-14 09:23:56 +0800

    feat(components): 修改

---
+++ dist/assets/index-021716af.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-6ee8c0e3.js";import"./vendor-b8cfd248.js";const o=()=>r("div",{children:"部门管理"});export{o as default};
---
+++ dist/assets/index-3e20610a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-6ee8c0e3.js";import"./vendor-b8cfd248.js";const a=()=>r("div",{children:"用户管理"});export{a as default};
