6afcae2b170d1e21d11f9d5f6b50d49652198a66
XiaoPiHong <XiaoPiHong@users.noreply.github.com>
2024-07-31 11:56:14 +0000

    deploy: edff3a1cae02247efb36e45f6f8b34491f6a63ca

---
+++ assets/index-45b918a4.js
@@ -0,0 +1,1 @@
+import{a as o,j as r}from"./index-7808db11.js";import{I as s}from"./vendor-61e85d3d.js";const e=()=>(console.log("部门管理"),o("div",{children:[r(s,{}),"部门管理"]}));export{e as default};
---
+++ assets/index-7ce9de01.js
@@ -0,0 +1,1 @@
+import{a as o,j as s}from"./index-7808db11.js";import{I as r}from"./vendor-61e85d3d.js";const e=()=>(console.log("角色管理"),o("div",{children:[s(r,{}),"角色管理"]}));export{e as default};
