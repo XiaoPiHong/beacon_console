1d8f023bd1f90496e86d851f26a05d41552774eb
XiaoPiHong <XiaoPiHong@users.noreply.github.com>
2024-08-04 06:50:17 +0000

    deploy: af693d5ea850b06b0616eb4260d26686070df8a1

--- assets/index-8a7edcb9.js
+++
@@ -1,1 +0,0 @@
-import{a as s,j as o}from"./index-e2369d7a.js";import{I as r}from"./vendor-1578c98f.js";const t=()=>(console.log("用户管理"),s("div",{children:[o(r,{}),"用户管理"]}));export{t as default};
---
+++ assets/index-99a663ab.js
@@ -0,0 +1,1 @@
+import{a as o,j as s}from"./index-2c976501.js";import{I as r}from"./vendor-7c011dec.js";const e=()=>(console.log("角色管理"),o("div",{children:[s(r,{}),"角色管理"]}));export{e as default};
